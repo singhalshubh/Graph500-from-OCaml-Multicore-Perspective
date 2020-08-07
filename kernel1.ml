f2a6709458e567094df344c4276fa7a69186b2fc
Shubhendra Pal Singhal <shubhendrapalsinghal@gmail.com>
2020-08-07 17:04:18 +0530

    Kernel-1 Adj Matrix completed
    -Comments remaining

--- kernel1.ml
+++ kernel1.ml
@@ -5,12 +5,36 @@ let rec transpose list col newList =
 		in transpose list (col+1) (newList@[(transposeRow 0 [])])  
 ;;
 
+let rec sortVerticeList list newList =
+	let rec sortVerticeList list newList maximum = 
+		match list with
+		[] -> (newList,maximum) |
+		head::tail -> let x = List.nth head 0 and y = List.nth head 1 in if x > y then sortVerticeList tail (newList@[[x;y;List.nth head 2]]) (max maximum x)     
+		else sortVerticeList tail (newList@[[y;x;List.nth head 2]]) (max maximum y)
+	in sortVerticeList list newList 0
+;;
+
 let rec removeSelfLoops ijw newList col m =
-	if col = m then (transpose newList 0 []) else begin
+	if col = m then newList else begin
 		if  List.nth (List.nth ijw 0) col = List.nth (List.nth ijw 1) col 
 		then removeSelfLoops ijw newList (col+1) m 
 	else removeSelfLoops ijw (newList@[ [(List.nth (List.nth ijw 0) col) ; (List.nth (List.nth ijw 1) col) ; (List.nth (List.nth ijw 2) col) ] ]) (col+1) m
 	end 
 ;;
 
-let rec kernel1 ijw = 
+let constructionAdjMatrix list maxLabel =
+	 let matrix = Array.make_matrix maxLabel maxLabel 0. in
+	 let rec fillMatrix matrix list =
+	 	match list with
+	 	[] -> matrix |
+	 	head::tail -> let _ = matrix.(int_of_float(List.nth head 0)).(int_of_float(List.nth head 1)) <- (List.nth head 2) in  
+	 				let _ = matrix.(int_of_float(List.nth head 1)).(int_of_float(List.nth head 0)) <- (List.nth head 2) in fillMatrix matrix tail
+	in fillMatrix matrix list
+;;	     
+
+let rec kernel1 ijw m = 
+	let list = removeSelfLoops ijw [] 0 m in
+	let list,maximumEdgeLabel = sortVerticeList list [] in
+	let adjMatrix = constructionAdjMatrix list (maximumEdgeLabel+1) in adjMatrix
+
+	
