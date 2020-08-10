c4ab3529d9eca7775427417b52f0eeaed2b4bc00
Shubhendra Pal Singhal <shubhendrapalsinghal@gmail.com>
2020-08-10 18:15:17 +0530

    Added sparse matrix by using Hash Table module
    - List of list would take a lot of time to access. Array would be difficult to store, so the best possible way was Hash Table. Also, the accumarray function had to be included otherwise the list(startVertex, endVertex, weight) would have been fine too.

--- kernel1.ml
+++ kernel1.ml
@@ -22,7 +22,7 @@ let rec removeSelfLoops ijw newList col m =
 	end 
 ;;
 
-let constructionAdjMatrix list maxLabel =
+(*let constructionAdjMatrix list maxLabel =
 	 let matrix = Array.make_matrix maxLabel maxLabel 0. in
 	 let rec fillMatrix matrix list =
 	 	match list with
@@ -30,11 +30,25 @@ let constructionAdjMatrix list maxLabel =
 	 	head::tail -> let _ = matrix.(int_of_float(List.nth head 0)).(int_of_float(List.nth head 1)) <- (List.nth head 2) in  
 	 				let _ = matrix.(int_of_float(List.nth head 1)).(int_of_float(List.nth head 0)) <- (List.nth head 2) in fillMatrix matrix tail
 	in fillMatrix matrix list
-;;	     
+;;*)
+
+let addEdge startVertex endVertex weight hashTable =
+	if Hashtbl.mem hashTable startVertex = false
+	then let _= Hashtbl.add hashTable startVertex [(endVertex,weight)] in hashTable
+	else let _ = Hashtbl.replace hashTable startVertex ( (Hashtbl.find hashTable startVertex) @ [(endVertex,weight)]) in hashTable
+;;
+
+
+let rec constructionAdjHash list hashTable = 
+	match list with 
+	[] -> hashTable |
+	head::tail -> let startVertex = List.nth head 0 and endVertex = List.nth head 1 and weight = List.nth head 2 in
+					let h = addEdge startVertex endVertex weight hashTable in let hashTable = addEdge endVertex startVertex weight h in constructionAdjHash tail hashTable 
+;;
 
 let rec kernel1 ijw m = 
 	let list = removeSelfLoops ijw [] 0 m in
 	let list,maximumEdgeLabel = sortVerticeList list [] in
-	let adjMatrix = constructionAdjMatrix list (maximumEdgeLabel+1) in adjMatrix
-
-	
+	let hashTable = Hashtbl.create size in
+	let adjMatrix = constructionAdjHash list hashTable in adjMatrix
+;;
