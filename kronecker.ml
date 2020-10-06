eb77a79edf29d20a5ed2038c1d2f254140f66605
Sudha Parimala <sudharg247@gmail.com>
2020-10-06 17:32:27 +0530

    refactored kronecker

--- kronecker.ml
+++ kronecker.ml
@@ -31,38 +31,45 @@ let scale = try int_of_string Sys.argv.(1) with _ -> 2
 
 let edgefactor = try int_of_string Sys.argv.(2) with _ -> 1
 
+let map f l =
+  let rec map_aux acc = function
+    | [] -> acc []
+    | x :: xs -> map_aux (fun ys -> acc (f x :: ys)) xs
+  in
+  map_aux (fun ys -> ys) l
 
-let rec listGenerator1D list m =
-  if m = 0 then list else listGenerator1D (0. :: list) (m - 1)
+let listGenerator m =
+  List.init 3 (fun _ -> List.init m (fun _ -> 0.))
 
-let rec listGenerator m list =
-  if List.length list = 3 then list
-  else listGenerator m (listGenerator1D [] m :: list)
+let randomWghtGen len =
+  List.init len (fun _ -> Random.float 1.)
+  (* if len = 0 then list else randomWghtGen (len - 1) (Random.float 1. :: list) *)
 
-let rec randomWghtGen len list =
-  if len = 0 then list else randomWghtGen (len - 1) (Random.float 1. :: list)
+let generateIIBitList m ab =
+  let rec aux acc n =
+    if n = 0 then acc 
+    else begin 
+      match (Random.float @@ float_of_int m) > ab with
+      | true -> aux (1. :: acc) (n - 1)
+      | false -> aux (0. :: acc) (n - 1)
+    end
+  in 
+  aux [] m
 
-let rec generateIIBitList list m ab =
-  if List.length list = m then List.rev list
-  else if Random.float (float_of_int m) > ab then
-    generateIIBitList (1. :: list) m ab
-  else generateIIBitList (0. :: list) m ab
-
-let rec generateJJBitList list ii_bit m a_norm c_norm =
-  match ii_bit with
-  | [] -> List.rev list
-  | head :: tail ->
-      if
-        Random.float (float_of_int m)
-        > (c_norm *. head) +. (a_norm *. float_of_int (int_of_float head lxor 1))
-      then generateJJBitList (1. :: list) tail m a_norm c_norm
-      else generateJJBitList (0. :: list) tail m a_norm c_norm
+let generateJJBitList ii_bit m a_norm c_norm =
+  let cmp a b = if
+    Random.float (float_of_int a)
+    > (c_norm *. b) +. (a_norm *. float_of_int (int_of_float b lxor 1))
+  then 1.
+  else 0.
+  in
+  map (cmp m) ii_bit 
 
 let rec modifyRowIJW kk_list index list newList =
   match (kk_list, list) with
   | [], [] -> List.rev newList
-  | _ :: _, [] -> []
-  | [], _ :: _ -> []
+  | _, [] -> []
+  | [], _ -> []
   | headII :: tailII, headList :: tailList ->
       let element = headList +. ((2. ** float_of_int index) *. headII) in
       modifyRowIJW tailII index tailList (element :: newList)
@@ -70,27 +77,23 @@ let rec modifyRowIJW kk_list index list newList =
 let rec compareWithPr index m n ab a_norm c_norm ijw scale =
   if index = scale then ijw
   else
-    let ii_bit = generateIIBitList [] m ab in
-    let jj_bit = generateJJBitList [] ii_bit m a_norm c_norm in
+    let ii_bit = generateIIBitList m ab in
+    let jj_bit = generateJJBitList ii_bit m a_norm c_norm in
     let firstRowIJW = modifyRowIJW ii_bit index (List.nth ijw 0) [] in
     let secondRowIJW = modifyRowIJW jj_bit index (List.nth ijw 1) [] in
     let ijw = [ firstRowIJW ] @ [ secondRowIJW ] @ [ List.nth ijw 2 ] in
     compareWithPr (index + 1) m n ab a_norm c_norm ijw scale
 
-let permute list =
-  let list = List.map (fun x -> (Random.bits (), x)) list in
+let permute list = 
+  let list = map (fun x -> (Random.bits (), x)) list in
   let list = List.sort compare list in
-  List.map (fun x -> snd x) list
+  map (fun x -> snd x) list
 
-let rec transpose col list newList =
-  if col = List.length (List.nth list 0) then newList
-  else
-    let rec adjustRow list newList col =
-      match list with
-      | [] -> List.rev newList
-      | head :: tail -> adjustRow tail (List.nth head col :: newList) col
-    in
-    transpose (col + 1) list (newList @ [ adjustRow list [] col ])
+  let  transpose ls =
+  let rec transpose_rec acc = function
+  | [] | [] :: _ -> List.rev acc
+  | ls -> transpose_rec (map (List.hd) ls :: acc) (map (List.tl) ls)
+  in transpose_rec [] ls
 
 let rec printList list =
   let _ = Printf.printf "\n" in
@@ -113,24 +116,24 @@ let computeNumber scale edgefactor =
 let kronecker scale edgefactor =
   let n, m = computeNumber scale edgefactor in
   let a, b, c = (0.57, 0.19, 0.19) in
-  let ijw = listGenerator m [] in
+  let ijw = listGenerator m in
   (*For debugging*)
-  let _ = printList ijw in
+  (* let _ = printList ijw in *)
   let ab = a +. b in
   let c_norm = c /. (1. -. (a +. b)) in
   let a_norm = a /. (a +. b) in
   let ijw = compareWithPr 0 m n ab a_norm c_norm ijw scale in
   (*For debugging*)
-  let _ = printList ijw in
-  let thirdRow = randomWghtGen m [] in
+  (* let _ = printList ijw in *)
+  let thirdRow = randomWghtGen m in
   let ijw = [ List.nth ijw 0 ] @ [ List.nth ijw 1 ] @ [ thirdRow ] in
   let firstRowPermute = permute (List.nth ijw 0) in
   let secondRowPermute = permute (List.nth ijw 1) in
   let ijw = [ firstRowPermute ] @ [ secondRowPermute ] @ [ List.nth ijw 2 ] in
   (*For debugging*)
-  let _ = printList ijw in
-  let ijw = permute (transpose 0 ijw []) in
-  let ijw = transpose 0 ijw [] in
+  (* let _ = printList ijw in *)
+  let ijw = permute (transpose ijw) in
+  let ijw = transpose ijw in
   let _ = Sys.remove "kronecker.txt" in
   let file = open_out "kronecker.txt" in
   let _ = writeFile ijw file in
