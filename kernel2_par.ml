d31ec462174d08d90305ee57a7038f8976d08adc
Shubhendra Pal Singhal <shubhendrapalsinghal@gmail.com>
2020-12-14 15:06:43 +0530

    CSR port of kernels.
    Works for directed graphs.

--- kernel2_par.ml
+++ kernel2_par.ml
@@ -13,51 +13,55 @@ module T = Domainslib.Task
 	[] -> None |
 	hd::tl -> Printf.printf "%d" (fst hd); printList tl
 *)
-let rec bfs adjMatrix level queue pool = 
+
+let rec findlst lst col row root index = 
+	if index = (row.(root+1)+1) then lst 
+else
+	if col.(index) = root then 
+	findlst lst col row root (index+1) else
+	findlst (col.(index)::lst) col row root (index+1)
+
+let rec bfs col row level queue pool = 
 	if Lockfree.MSQueue.is_empty queue = true then ()
 else
 	match Lockfree.MSQueue.pop queue with
 	None -> () |
 	Some root -> 
-		match Lockfree.Hash.find adjMatrix root with
-			None -> () |
-			Some lst ->
-				(*Printf.printf "Root : %d\n" root;
-				let _ = Array.iter (fun i -> Printf.printf "%d" i) level in
-				Printf.printf "\n"; 
-				let _ = printList lst in*)
-				(*Printf.printf "\n";*)
+		let lst = findlst [] col row root (row.(root)) in 
+		Printf.printf "Root : %d\n" root;
+		let _ = Array.iter (fun i -> Printf.printf "%d" i) level in
+		Printf.printf "\n"; 
+		let _ = List.iter (fun i -> Printf.printf "%d" i) lst in
+		Printf.printf "\n";
 
-				T.parallel_for pool ~start:0 ~finish:(List.length lst - 1) 
-				~body:(	fun i -> 
-						(*Printf.printf "INdex : %d " i;*)
-						if level.((fst (List.nth lst i))) != (-1) then ()
-								 else begin 
-									(*Printf.printf "efjewfjef\n";*)
-									level.((fst (List.nth lst i))) <- level.(root) + 1;
-									(*Printf.printf "jk";*)
-									Lockfree.MSQueue.push queue (fst (List.nth lst i))
-									end 
-				);
-				bfs adjMatrix level queue pool
+		T.parallel_for pool ~start:0 ~finish:(List.length lst - 1) 
+		~body:(	fun i -> 
+				(*Printf.printf "INdex : %d " i;*)
+				if level.(List.nth lst i) != (-1) then ()
+						 else begin 
+							(*Printf.printf "efjewfjef\n";*)
+							level.(List.nth lst i) <- level.(root) + 1;
+							(*Printf.printf "jk";*)
+							Lockfree.MSQueue.push queue (List.nth lst i)
+							end 
+		);
+		bfs col row level queue pool
 
 let kernel2 () = 
-  	let ans = Kernel1_par.linkKronecker () in
-  	let adjMatrix = fst ans in
-  	let n = snd ans in
-  	(*let s = Unix.gettimeofday () in*)
+  	let (_, col, row, n) = Kernel1_csr.linkKronecker () in
+  	let s = Unix.gettimeofday () in
   	let level = Array.make n (-1) in
   	level.(startVertex) <- 0;
 	let queue = Lockfree.MSQueue.create () in
 	let _ = Lockfree.MSQueue.push queue startVertex in
 	let pool = T.setup_pool ~num_domains:(num_domains - 1) in
 	let t = Unix.gettimeofday () in
-	let _ = bfs adjMatrix level queue pool in
+	let _ = bfs col row level queue pool in
 	let r = Unix.gettimeofday () in
 	Printf.printf "\nBFS: %f\n" (r -. t);
-	(*Printf.printf "\nKERNEL2 TOTAL: %f\n" (r -. s);*)
+	Printf.printf "\nKERNEL2 TOTAL: %f\n" (r -. s);
 	T.teardown_pool pool;
-	(*let _ = Array.iter (fun i -> Printf.printf "%d" i) level in*)
+	let _ = Array.iter (fun i -> Printf.printf "%d" i) level in
 	level
 
 let _ = kernel2 ()
--- kernel3_par.ml
+++ kernel3_par.ml
@@ -8,7 +8,14 @@ let num_domains = try int_of_string Sys.argv.(3) with _ -> 1
 
 module T = Domainslib.Task
 
-let rec sssp adjMatrix visited queue distance pool prev_root parent = 
+let rec findlst lst value col row root index = 
+	if index = (row.(root+1)+1) then lst 
+else
+	if col.(index) = root then 
+	findlst lst value col row root (index+1) else
+	findlst ( (col.(index), value.(index)) :: lst) value col row root (index+1)
+
+let rec sssp value col row visited queue distance pool prev_root parent = 
 	match Lockfree.List.elem_of queue with
 	[] -> () |
 	(droot, root)::_ ->
@@ -19,44 +26,41 @@ let rec sssp adjMatrix visited queue distance pool prev_root parent =
 		Printf.printf "Elements of Queue : ";*)
 		parent.(root) <- prev_root;
 		(*List.iter (fun (x,y) -> Printf.printf " %f , %d " x y) (Lockfree.List.elem_of queue);*)
-		match Lockfree.Hash.find adjMatrix root with
-			None -> () |
-			Some lst ->
-				let ar = Array.of_list lst in
-				let dist = Atomic.get distance in
-				(*Printf.printf "Adj List for Root : ";
-				Array.iter (fun (x,y) -> Printf.printf "%d,%f " x y) ar;
-				Printf.printf "Distance : ";
-				Array.iter (fun i -> Printf.printf "%f " i) dist;*)
-				T.parallel_for pool ~start:0 ~finish:(List.length lst - 1) 
-				~body:(	fun i ->
-						if visited.(fst ar.(i)) = 0 then 
-								dist.(fst ar.(i)) <- min (dist.(fst ar.(i))) ( dist.(root) +. (snd ar.(i)) )
-					else ()
-				);
-				(*Printf.printf "Updated Distance : ";
-				Array.iter (fun(x) -> Printf.printf "%f " x ) dist;
-				Printf.printf "\n";*)
-				T.parallel_for pool ~start:0 ~finish:(List.length lst - 1) 
-				~body:(	fun i -> 
-						if visited.(fst ar.(i)) = 0 then
-							match Lockfree.List.sinsert queue (dist.(fst ar.(i)), fst ar.(i)) compare with
-								false, _ -> () |
-								true, _ -> () (* Printf.printf "%d" (fst ar.(i)) *)
-					else ()
-				);
-				(*Printf.printf "[QUEUE Elements at end] :  ";
-				List.iter (fun (x,y) -> Printf.printf " %f,%d " x y) (Lockfree.List.elem_of queue);
-				Printf.printf "\n";
-				Printf.printf "\n";*)
-				visited.(root) <- 1;
-				sssp adjMatrix visited queue distance pool root parent
+		let lst = findlst [] value col row root (row.(root)) in 
+		let ar = Array.of_list lst in
+		(*if (Array.length ar) < 12 then 
+			Printf.printf "%d \n" (Array.length ar);*)
+		let dist = Atomic.get distance in
+		(*Printf.printf "Adj List for Root : ";
+		Array.iter (fun (x,y) -> Printf.printf "%d,%f " x y) ar;
+		Printf.printf "Distance : ";
+		Array.iter (fun i -> Printf.printf "%f " i) dist;*)
+		T.parallel_for pool ~start:0 ~finish:(List.length lst - 1) 
+		~body:(	fun i ->
+				if visited.(fst ar.(i)) = 0 then 
+						dist.(fst ar.(i)) <- min (dist.(fst ar.(i))) ( dist.(root) +. (snd ar.(i)) )
+			else ()
+		);
+		(*Printf.printf "Updated Distance : ";
+		Array.iter (fun(x) -> Printf.printf "%f " x ) dist;
+		Printf.printf "\n";*)
+		T.parallel_for pool ~start:0 ~finish:(List.length lst - 1) 
+		~body:(	fun i -> 
+				if visited.(fst ar.(i)) = 0 then
+					match Lockfree.List.sinsert queue (dist.(fst ar.(i)), fst ar.(i)) compare with
+						false, _ -> () |
+						true, _ -> () (* Printf.printf "%d" (fst ar.(i)) *)
+			else ()
+		);
+		(*Printf.printf "[QUEUE Elements at end] :  ";
+		List.iter (fun (x,y) -> Printf.printf " %f,%d " x y) (Lockfree.List.elem_of queue);
+		Printf.printf "\n";
+		Printf.printf "\n";*)
+		visited.(root) <- 1;
+		sssp value col row visited queue distance pool root parent
 
 let kernel3 () = 
-  	let ans = Kernel1_par.linkKronecker () in
-  	let adjMatrix = fst ans in
-  	let n = snd ans in
-  	(*let s = Unix.gettimeofday () in*)
+  	let (value, col, row, n) = Kernel1_csr.linkKronecker () in
   	let visited = Array.make n (0) in
   	let parent = Array.make n (-1) in
   	let distance = Array.make n (infinity) in
@@ -67,15 +71,15 @@ let kernel3 () =
 	true,_ -> 
 		let pool = T.setup_pool ~num_domains:(num_domains - 1) in
 		let t = Unix.gettimeofday () in
-		let _ = sssp adjMatrix visited queue (Atomic.make distance) pool (-1) parent in
+		let _ = sssp value col row visited queue (Atomic.make distance) pool (-1) parent in
 		let r = Unix.gettimeofday () in
 		Printf.printf "\nSSSP: %f" (r -. t);
 		(*Printf.printf "\nKERNEL3 TOTAL: %f\n" (r -. s);*)
 		T.teardown_pool pool;
-		(*Printf.printf "Distance Array is : ";
+		Printf.printf "Distance Array is : ";
 		let _ = Array.iter (fun i -> Printf.printf "%f " i) distance in
 		Printf.printf "\n Parent Array is : ";
-		let _ = Array.iter (fun i -> Printf.printf "%d " i) parent in*)
+		let _ = Array.iter (fun i -> Printf.printf "%d " i) parent in
 		distance,parent
 
 let _ = kernel3 ()
